74c6b1dd3c6c32f564db36be9359e09b8a353d6a
David Castagneto A <davidcastagnetoa@gmail.com>
2023-02-05 07:50:43 +0100

    server cors bug fixed error 204 OPTIONS response

--- index.js
+++ index.js
@@ -29,7 +29,13 @@ app.use(helmet.crossOriginResourcePolicy({ policy: "cross-origin" }));
 app.use(morgan("common"));
 app.use(bodyParser.json({ limit: "30mb", extended: true }));
 app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
-app.use(cors());
+// app.use(cors());
+// solo permite solicitudes desde la aplicación web
+app.use(
+  cors({
+    origin: ["https://sociopathmedia.netlify.app/"],
+  })
+);
 
 //Set the directory where we keep or assets (in real app it's to store in actual store file directry or in S3)
 app.use("/assets", express.static(path.join(__dirname, "public/assets")));
@@ -45,12 +51,11 @@ const storage = multer.diskStorage({
 });
 const upload = multer({ storage });
 
-
 // ROUTES WITH FILES
 app.post("/auth/register", upload.single("picture"), register);
 app.post("/posts", verifyToken, upload.single("picture"), createPost);
 
-// ROUTES 
+// ROUTES
 app.use("/auth", authRoutes);
 app.use("/users", userRoutes);
 app.use("/posts", postRoutes);
