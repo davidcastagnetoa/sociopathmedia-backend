53e7dfb169051ca490de79e7e57d721e772d6ded
David Castagneto A <davidcastagnetoa@gmail.com>
2023-03-10 12:59:51 +0100

    no login google

--- controllers/auth.js
+++ controllers/auth.js
@@ -2,7 +2,6 @@ import bcrypt from "bcrypt";
 import jwt from "jsonwebtoken";
 import User from "../models/User.js";
 
-
 /* REGISTER USER */
 export const register = async (req, res) => {
   try {
@@ -41,84 +40,6 @@ export const register = async (req, res) => {
   }
 };
 
-// Original
-/* LOGGING IN */
-// export const login = async (req, res) => {
-//   try {
-//     const { email, password } = req.body;
-//     console.log('Request body:', req.body); // Agregado para depuración
-//     const user = await User.findOne({ email: email });
-//     if (!user) return res.status(400).json({ msg: "User does not exist. " });
-
-//     const isMatch = await bcrypt.compare(password, user.password);
-//     if (!isMatch) return res.status(400).json({ msg: "Invalid credentials. " });
-
-//     const token = jwt.sign({ id: user._id }, process.env.JWT_SECRET);
-//     delete user.password;
-//     res.status(200).json({ token, user });
-//   } catch (err) {
-//     console.log(err); // Agregado para depuración
-//     res.status(500).json({ error: err.message });
-//   }
-// };
-
-// with Google
-/* LOGGING IN */
-export const login = async (req, res) => {
-  try {
-    const { email, password, token: googleToken } = req.body;
-    console.log("Request body:", req.body); // Agregado para depuración
-    const user = await User.findOne({ email: email });
-    if (!user) {
-      if (!googleToken) {
-        return res.status(400).json({ msg: "User does not exist. " });
-      }
-      // Si el usuario no existe pero se recibió un token de Google, se crea una cuenta nueva
-      const client = new OAuth2Client(process.env.GOOGLE_CLIENT_ID);
-      const ticket = await client.verifyIdToken({
-        idToken: googleToken,
-        audience: process.env.GOOGLE_CLIENT_ID,
-      });
-      const { given_name, family_name, email, picture } = ticket.getPayload();
-      const password = Math.random().toString(36).substring(7);
-      const salt = await bcrypt.genSalt();
-      const passwordHash = await bcrypt.hash(password, salt);
-      const newUser = new User({
-        firstName: given_name,
-        lastName: family_name,
-        email,
-        password: passwordHash,
-        picturePath: picture,
-        friends: [],
-        location: "",
-        occupation: "",
-        visits: 0,
-        impressions: Math.floor(Math.random() * 10000),
-      });
-      const savedUser = await newUser.save();
-      const token = jwt.sign({ id: savedUser._id }, process.env.JWT_SECRET);
-      delete savedUser.password;
-      res.status(200).json({ token, user: savedUser });
-      return;
-    }
-
-    const isMatch = await bcrypt.compare(password, user.password);
-    if (!isMatch) return res.status(400).json({ msg: "Invalid credentials. " });
-
-    const token = jwt.sign({ id: user._id }, process.env.JWT_SECRET);
-    delete user.password;
-    res.status(200).json({ token, user });
-  } catch (err) {
-    console.log(err); // Agregado para depuración
-    res.status(500).json({ error: err.message });
-  }
-};
-
-// CREATE USER IN MONGODB IF USER DOESN'T EXIST
-
-/* LOGGING WITH GOOGLE */
-
-
 // Increment view count
 // export const incrementViewCount = async (req, res) => {
 //   try {
@@ -132,3 +53,21 @@ export const login = async (req, res) => {
 //     res.status(500).json({ error: err.message });
 //   }
 // };
+
+/* LOGGING IN */
+export const login = async (req, res) => {
+  try {
+    const { email, password } = req.body;
+    const user = await User.findOne({ email: email });
+    if (!user) return res.status(400).json({ msg: "User does not exist. " });
+
+    const isMatch = await bcrypt.compare(password, user.password);
+    if (!isMatch) return res.status(400).json({ msg: "Invalid credentials. " });
+
+    const token = jwt.sign({ id: user._id }, process.env.JWT_SECRET);
+    delete user.password;
+    res.status(200).json({ token, user });
+  } catch (err) {
+    res.status(500).json({ error: err.message });
+  }
+};
